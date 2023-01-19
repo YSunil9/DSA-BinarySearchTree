a63afbd0c8d9d7cff2a9e99b234a6aa7c7fe55a9
Ysunil9 <sunil.pcs24@gmail.com>
2023-01-19 23:21:51 +0530

    [SUNIL]:Created A BST

---
+++ DSA_BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_BinarySearchTree
+{
+    internal class BinarySearchTree<T> where T : IComparable<T>
+    {
+            public T Data;
+            public BinarySearchTree<T> left;
+            public BinarySearchTree<T> right;
+            public BinarySearchTree(T data)
+            {
+                this.Data = data;
+                this.left = null;
+                this.right = null;
+            }
+            int leftIndex = 0, rightIndex = 0;
+            public void AddNode(T value)
+            {
+                T Nodevalue = this.Data;
+                if (Nodevalue.CompareTo(value) > 0)
+                {
+                    if (this.left == null) this.left = new BinarySearchTree<T>(value);
+                    else
+                        this.left.AddNode(value);
+                }
+                else
+                {
+                    if (this.right == null) this.right = new BinarySearchTree<T>(value);
+                    else
+                        this.right.AddNode(value);
+                }
+            }
+            public void Display()
+            {
+                if (this.left != null)
+                {
+                    this.leftIndex++;
+                    this.left.Display();
+                }
+                Console.WriteLine(this.Data);
+                if (this.right != null)
+                {
+                    this.rightIndex++;
+                    this.right.Display();
+                }
+            } 
+    }
+}
+
---
+++ DSA_BinarySearchTree/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DSA_BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>(56);
+            binarySearchTree.AddNode(30);
+            binarySearchTree.AddNode(70);
+            binarySearchTree.Display();
+        }
+    }
+}
